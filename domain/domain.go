1cf24a47ff2112d3154d7386d2e32d874657933b
Czarek <sarek123456789@gmail.com>
2019-09-04 19:24:36 +0200

    Create ProductId type = uint32

--- domain/domain.go
+++ domain/domain.go
@@ -1,29 +1,31 @@
 package domain
 
+type ProductId = uint32
+
 type Product struct {
-	ProductId   int
+	ProductID   ProductId
 	Title       string
 	Sku         string
 	Barcodes    []int
 	Description string
 	Price       string
-	Created     int
-	LastUpdated int
+	Created     uint32
+	LastUpdated uint32
 }
 
 type ProductService interface {
 	GetProducts(start uint64, num uint64, sku string, barcode string, fields []string) ([]Product, error)
-	GetProduct(id int, fields []string) (Product, error)
+	GetProduct(id ProductId, fields []string) (Product, error)
 	AddProduct(product Product) (int, error)
-	UpdateProduct(id int, product Product) error
-	DeleteProduct(id int) error
+	UpdateProduct(id ProductId, product Product) error
+	DeleteProduct(id ProductId) error
 }
 
 type ProductRepository interface {
 	GetProducts(start uint64, num uint64, sku string, barcode string) ([]Product, error)
-	GetProduct(id int) (Product, error)
-	AddProduct(product Product) (int, error)
-	UpdateProduct(id int, product Product) error
-	DeleteProduct(id int) error
+	GetProduct(id ProductId) (Product, error)
+	AddProduct(product Product) (ProductId, error)
+	UpdateProduct(id ProductId, product Product) error
+	DeleteProduct(id ProductId) error
 	SkuExists(sku string) (bool, error)
 }
--- services/services.go
+++ services/services.go
@@ -27,7 +27,7 @@ func (service Service) GetProducts(
 }
 
 func (service Service) GetProduct(
-	id int,
+	id domain.ProductId,
 	fields []string,
 ) (domain.Product, error) {
 
@@ -47,25 +47,25 @@ func (service Service) GetProduct(
 
 func (service Service) AddProduct(
 	product domain.Product,
-) (int, error) {
+) (domain.ProductId, error) {
 
 	log.Println("Adding product")
 
 	exists, err := service.repo.SkuExists(product.Sku)
 
 	if err != nil {
-		return -1, err
+		return 0, err
 	}
 
 	if exists {
-		return -1, errors.New(fmt.Sprintf("SKU '%s' already exists", product.Sku))
+		return 0, errors.New(fmt.Sprintf("SKU '%s' already exists", product.Sku))
 	}
 
 	return service.repo.AddProduct(product)
 }
 
 func (service Service) UpdateProduct(
-	id int,
+	id domain.ProductId,
 	product domain.Product,
 ) error {
 
@@ -75,7 +75,7 @@ func (service Service) UpdateProduct(
 }
 
 func (service Service) DeleteProduct(
-	id int,
+	id domain.ProductId,
 ) error {
 
 	return service.repo.DeleteProduct(id)
