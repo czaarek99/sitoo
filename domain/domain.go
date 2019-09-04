b61f362bfd2699866287d3b415538251229de4f2
Czarek <sarek123456789@gmail.com>
2019-09-04 21:51:30 +0200

    Add ProductUpdateInput

--- domain/domain.go
+++ domain/domain.go
@@ -28,11 +28,20 @@ type ProductAddInput struct {
 	Attributes  []ProductAttribute
 }
 
+type ProductUpdateInput struct {
+	Title       *string
+	Sku         *string
+	Barcodes    *[]string
+	Description *string
+	Price       *string
+	Attributes  *[]ProductAttribute
+}
+
 type ProductService interface {
 	GetProducts(start uint64, num uint64, sku string, barcode string, fields []string) ([]Product, error)
 	GetProduct(id ProductId, fields []string) (Product, error)
 	AddProduct(product ProductAddInput) (ProductId, error)
-	UpdateProduct(id ProductId, product Product) error
+	UpdateProduct(id ProductId, product ProductUpdateInput) error
 	DeleteProduct(id ProductId) error
 }
 
@@ -40,7 +49,7 @@ type ProductRepository interface {
 	GetProducts(start uint64, num uint64, sku string, barcode string) ([]Product, error)
 	GetProduct(id ProductId) (Product, error)
 	AddProduct(product ProductAddInput) (ProductId, error)
-	UpdateProduct(id ProductId, product Product) error
+	UpdateProduct(id ProductId, product ProductUpdateInput) error
 	DeleteProduct(id ProductId) error
 	SkuExists(sku string) (bool, error)
 }
