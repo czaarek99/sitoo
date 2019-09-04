a86305a066667c9e0fbcad18fe5cfd61e88741b2
Cezary Borowski <cezary.borowski@lakarleasing.se>
2019-09-04 13:10:20 +0200

    Handle 404 and respond with json if impossible

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
+type ErrorResponse struct {
+	ErrorText string
+}
+
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello world")
+	writer.Header().Set("Content-Type", "application/json")
+
+	var response interface{}
+
+	if strings.HasPrefix(request.URL.Path, "/api/products") {
+
+	} else {
+		response = ErrorResponse{
+			ErrorText: "Route not found",
+		}
+	}
+
+	jsonBytes, err := json.Marshal(response)
+
+	if err == nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+	} else {
+		writer.Write(jsonBytes)
+	}
 
 }
 
 func main() {
+	fmt.Println("Starting server")
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
