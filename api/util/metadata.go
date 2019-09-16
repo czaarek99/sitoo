a2e9d0060b8d1bedac42fadd18aac5c45920c9c7
Cezary Borowski <cezary.borowski@lakarleasing.se>
2019-09-16 08:21:59 +0200

    Add comment to metadata.go

--- api/util/metadata.go
+++ api/util/metadata.go
@@ -1,5 +1,13 @@
 package util
 
+/*
+Currently the metadata only has a RequestID which is handy
+for tracking logs.
+
+In a production service we would probably have more information
+here as for example the user that is actually making
+the request.
+*/
 type Metadata struct {
 	RequestID uint32
 }
